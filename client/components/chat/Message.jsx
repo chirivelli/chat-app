37b3b5211f75288c686a5c37824df1226b123636
Sathwik <sathwikchirivelli@gmail.com>
2025-05-02 21:19:41 +0530

    minor ui improvement

--- client/components/chat/Message.jsx
+++ client/components/chat/Message.jsx
@@ -3,16 +3,16 @@ export default function Message({ user, message }) {
     <>
       <div className={`flex ${message.sender === user && 'justify-end'}`}>
         <div className='max-w-2/3 rounded-md border bg-slate-300'>
-          <div className='px-1 font-bold text-slate-950 italic'>
+          <div className='px-2 font-bold text-slate-950 italic'>
             {message.sender}
           </div>
 
           <div className='flex flex-wrap items-end justify-between'>
-            <div className='px-1 font-medium text-slate-900'>
+            <div className='px-2 font-medium text-slate-900'>
               {message.content}
             </div>
 
-            <div className='px-0.5 font-light text-slate-900/70'>
+            <div className='px-2 font-light text-slate-900/70'>
               {message.time}
             </div>
           </div>
